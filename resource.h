3f967a43cd69b9a88cd6dfaac7265bfcbc47cd90
BuckLuckDuck <nikitos80011@gmail.com>
2022-06-07 00:00:33 +0700

    Добавьте файлы проекта.

---
+++ resource.h
@@ -0,0 +1,40 @@
+﻿//{{NO_DEPENDENCIES}}
+// Включаемый файл, созданный в Microsoft Visual C++.
+// Используется System.programming.lab4.rc
+//
+#define IDC_MYICON                      2
+#define IDD_SYSTEMPROGRAMMINGLAB4_DIALOG 102
+#define IDS_APP_TITLE                   103
+#define IDD_ABOUTBOX                    103
+#define IDM_ABOUT                       104
+#define IDM_EXIT                        105
+#define IDI_SYSTEMPROGRAMMINGLAB4       107
+#define IDI_SMALL                       108
+#define IDC_SYSTEMPROGRAMMINGLAB4       109
+#define IDR_EL_ACCEL                    109
+#define IDR_MAINFRAME                   128
+#define ID_32771                        32771
+#define ID_COLOR                        32772
+#define ID_32773                        32773
+#define ID_32774                        32774
+#define ID_32775                        32775
+#define ID_DELETE                       32776
+#define ID_DRAW_MODE                    32777
+#define ID_SELECT_MODE                  32778
+#define ID_TOGGLE_MODE                  32780
+#define ID_MODE_SWITCH                  32780
+#define ID_MODE_DRAW                    32784
+#define ID_MODE_SELECT                  32785
+#define IDC_STATIC                      -1
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NO_MFC                     1
+#define _APS_NEXT_RESOURCE_VALUE        129
+#define _APS_NEXT_COMMAND_VALUE         32786
+#define _APS_NEXT_CONTROL_VALUE         1000
+#define _APS_NEXT_SYMED_VALUE           110
+#endif
+#endif
